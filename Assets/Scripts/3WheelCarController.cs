e929c331a3533ac6ffad9663471ed31e5c9181ff
phobosnya <andreyludnikov@gmail.com>
2024-02-18 16:35:18 +0300

    refabricated control system

--- Assets/Scripts/3WheelCarController.cs
+++ Assets/Scripts/3WheelCarController.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class carMovement : MonoBehaviour {
-    private float MotoForce_3 = 4000.0f;  // мощность вращения передних колес
-    private float SteerForce_3 = 60.0f; // угол поворота передних колес
+    private float MotoForce_3 = 8000f;  // мощность вращения передних колес
 
     public WheelCollider CWFR_3;
     public WheelCollider CWFL_3;
@@ -14,12 +13,50 @@ public class carMovement : MonoBehaviour {
 
     void FixedUpdate() {
         float v = Input.GetAxis("Vertical") * MotoForce_3; // ускорение колес
-        float h = Input.GetAxis("Horizontal") * SteerForce_3; // угол поворотaw передних колес
 
         TWFR_3.Rotate((CWFR_3.rpm*3) / 60 * 360 * Time.deltaTime, 0.0f, 0.0f);
         TWFL_3.Rotate((CWFL_3.rpm*3) / 60 * 360 * Time.deltaTime, 0.0f, 0.0f);
 
-
+        if (Input.GetKey(KeyCode.W))
+        {
+            if (Input.GetKey(KeyCode.A))
+            {
+                CWFR_3.motorTorque = 0f;
+                CWFL_3.motorTorque = v;
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                CWFR_3.motorTorque = v;
+                CWFL_3.motorTorque = 0f;
+            }
+            else
+            {
+                CWFR_3.motorTorque = v;
+                CWFL_3.motorTorque = v;
+            }
+        }
+        else if (Input.GetKey(KeyCode.S)){
+            if (Input.GetKey(KeyCode.A))
+            {
+                CWFR_3.motorTorque = 0f;
+                CWFL_3.motorTorque = v;
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                CWFR_3.motorTorque = v;
+                CWFL_3.motorTorque = 0f;
+            }
+            else
+            {
+                CWFR_3.motorTorque = v;
+                CWFL_3.motorTorque = v;
+            }
+        }
+        else
+        {
+            CWFL_3.motorTorque = 0f;
+            CWFR_3.motorTorque = 0f;
+        }
         if (Input.GetKey(KeyCode.Space)) {
             CWFL_3.brakeTorque = 3000f;
             CWFR_3.brakeTorque = 3000f;
@@ -29,10 +66,5 @@ public class carMovement : MonoBehaviour {
             CWFR_3.brakeTorque = 0f;
         }
 
-        CWFR_3.motorTorque = v;
-        CWFL_3.motorTorque = v;
-
-        CWFR_3.steerAngle = h;
-        CWFL_3.steerAngle = h;
     }
 }
