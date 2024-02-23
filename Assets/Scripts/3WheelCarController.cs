3b52521336a2845af516b8a8c898592b8f25a7b2
phobosnya <andreyludnikov@gmail.com>
2024-02-23 12:34:30 +0300

    initial

--- Assets/Scripts/3WheelCarController.cs
+++ Assets/Scripts/3WheelCarController.cs
@@ -1,7 +1,8 @@
-﻿using UnityEngine;
+﻿using System.Threading.Tasks;
+using UnityEngine;
 
 public class carMovement : MonoBehaviour {
-    private float MotoForce_3 = 6000f;  // мощность вращения передних колес
+    private bool isMoving = false;
 
     public WheelCollider CWFR_3;
     public WheelCollider CWFL_3;
@@ -9,7 +10,7 @@ public class carMovement : MonoBehaviour {
     public Transform TWFR_3;
     public Transform TWFL_3;
 
-    void Movement(float lf, float rf, float t)
+    void Movement(float lf, float rf, int t)
     {
         float v1 = Input.GetAxis("Vertical") * lf;
         float v2 = Input.GetAxis("Vertical") * rf;
@@ -20,64 +21,18 @@ public class carMovement : MonoBehaviour {
         TWFR_3.Rotate((CWFR_3.rpm * 3) / 60 * 360 * Time.deltaTime, 0.0f, 0.0f);
         TWFL_3.Rotate((CWFL_3.rpm * 3) / 60 * 360 * Time.deltaTime, 0.0f, 0.0f);
 
-        if (Input.GetKey(KeyCode.W))
-        {
-            if (Input.GetKey(KeyCode.A))
-            {
-                CWFR_3.motorTorque = 0f;
-                CWFL_3.motorTorque = v1;
-            }
-            else if (Input.GetKey(KeyCode.D))
-            {
-                CWFR_3.motorTorque = v2;
-                CWFL_3.motorTorque = 0f;
-            }
-            else
-            {
-                CWFR_3.motorTorque = v2;
-                CWFL_3.motorTorque = v1;
-            }
-        }
-        else if (Input.GetKey(KeyCode.S))
-        {
-            if (Input.GetKey(KeyCode.A))
-            {
-                CWFR_3.motorTorque = 0f;
-                CWFL_3.motorTorque = v1;
-            }
-            else if (Input.GetKey(KeyCode.D))
-            {
-                CWFR_3.motorTorque = v2;
-                CWFL_3.motorTorque = 0f;
-            }
-            else
-            {
-                CWFR_3.motorTorque = v2;
-                CWFL_3.motorTorque = v1;
-            }
-        }
-        else if (Input.GetKey(KeyCode.A))
-        {
-            CWFR_3.motorTorque = 0f;
-            CWFL_3.motorTorque = -h1;
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            CWFL_3.motorTorque = 0f;
-            CWFR_3.motorTorque = h2;
-        }
-        else
-        {
-            CWFL_3.motorTorque = 0f;
-            CWFR_3.motorTorque = 0f;
-        }
+
+
     }
 
-    void FixedUpdate()
+    void Update()
     {
         float lf = 5000f;
         float rf = 5000f;
-        float t = 3;
-        Movement(lf, rf, t);
+        int t = 3;
+        if (isMoving || Input.anyKeyDown)
+        {
+            Movement(lf, rf, t);
+        }
     }
 }
