4de2eb3fb9a1fd0e63c04afceb1ae951cbb562a7
olegg <s.gelo2004@gmail.com>
2024-02-29 21:22:26 +0300

    demo middle script added

--- Assets/Scripts/CarMovement.cs
+++ Assets/Scripts/CarMovement.cs
@@ -1,12 +1,12 @@
 ﻿using System.Threading.Tasks;
 using UnityEngine;
 
-public class carMovement : MonoBehaviour {
-    private bool isMoving = false;
-    private bool w=false;
-    private bool s=false;
-    private bool a=false;
-    private bool d=false;
+public class CarMovement : MonoBehaviour {
+    public bool isMoving = false;
+    public bool w = false;
+    public bool s = false;
+    public bool a = false;
+    public bool d = false;
     public float starttime;
     public WheelCollider CWFR_3;
     public WheelCollider CWFL_3;
@@ -14,61 +14,52 @@ public class carMovement : MonoBehaviour {
     public Transform TWFR_3;
     public Transform TWFL_3;
 
-    void Movement(float lf, float rf, int timedelta)
-    {
+    public void Movement(float lf, float rf, int timedelta) {
         float v1 = lf;
         float v2 = rf;
 
 
         TWFR_3.Rotate((CWFR_3.rpm * 3) / 60 * 360 * Time.deltaTime, 0.0f, 0.0f);
         TWFL_3.Rotate((CWFL_3.rpm * 3) / 60 * 360 * Time.deltaTime, 0.0f, 0.0f);
-        
-        if (Input.GetKeyDown(KeyCode.W))
-        {
+
+        if (Input.GetKeyDown(KeyCode.W)) {
             w = true;
             isMoving = true;
             starttime = Time.time;
         }
-        else if(Input.GetKeyDown(KeyCode.S)){
+        else if (Input.GetKeyDown(KeyCode.S)) {
             s = true;
             isMoving = true;
             starttime = Time.time;
         }
-        else if (Input.GetKeyDown(KeyCode.A))
-        {
+        else if (Input.GetKeyDown(KeyCode.A)) {
             a = true;
             isMoving = true;
             starttime = Time.time;
         }
-        else if (Input.GetKeyDown(KeyCode.D))
-        {
+        else if (Input.GetKeyDown(KeyCode.D)) {
             d = true;
             isMoving = true;
             starttime = Time.time;
         }
 
-        if (isMoving && Time.time - starttime <= timedelta && w == true)
-        {
+        if (isMoving && Time.time - starttime <= timedelta && w == true) {
             CWFR_3.motorTorque = v2;
             CWFL_3.motorTorque = v1;
         }
-        else if(isMoving && Time.time - starttime <= timedelta && s == true)
-        {
+        else if (isMoving && Time.time - starttime <= timedelta && s == true) {
             CWFR_3.motorTorque = -v2;
             CWFL_3.motorTorque = -v1;
         }
-        else if (isMoving && Time.time - starttime <= timedelta && a == true)
-        {
+        else if (isMoving && Time.time - starttime <= timedelta && a == true) {
             CWFR_3.motorTorque = 0f;
             CWFL_3.motorTorque = v1;
         }
-        else if (isMoving && Time.time - starttime <= timedelta && d == true)
-        {
+        else if (isMoving && Time.time - starttime <= timedelta && d == true) {
             CWFR_3.motorTorque = v2;
             CWFL_3.motorTorque = 0f;
         }
-        else
-        {
+        else {
             isMoving = false;
             w = false;
             s = false;
@@ -76,17 +67,6 @@ public class carMovement : MonoBehaviour {
             d = false;
             CWFR_3.motorTorque = 0f;
             CWFL_3.motorTorque = 0f;
-        }
-    }
-
-    void Update()
-    {
-        float lf = 2000f;
-        float rf = 2000f;
-        int t = 10;
-        if (isMoving || Input.anyKeyDown)
-        {
-            Movement(lf, rf, t);
         }
     }
 }
---
+++ Assets/Scripts/middleScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class middleScript : MonoBehaviour
+{
+
+    public CarMovement carMovementScript; // Ссылка на объект класса carMovement
+
+    void Start() {
+        // Получаем ссылку на компонент СarMovement при старте
+        carMovementScript = GetComponent<CarMovement>();
+    }
+
+    private void Update() {
+        float lf = 2000f;
+        float rf = 2000f;
+        int t = 2;
+        if (carMovementScript.isMoving || Input.anyKeyDown) {
+            carMovementScript.Movement(lf, rf, t);
+        }
+    }
+    
+}
